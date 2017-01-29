6b1bc6e7c1833c670f83164e750a3cd8dee6dac4
Nathan Meyers <nathan@runnable.com>
2017-01-28 16:59:30 -0800

    Allow the joi to accept dot versions (#6)
    * Allow the joi to accept dot versions
    
    Switching from valid to a regex that works for 2 and 2.X
    
    * Needs the ? at the end
    
    * Add unit tests for parsing the version

---
+++ test/unit/index.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const DockerComposeParser = require('index')
+
+describe('index', () => {
+  describe('#_parseDockerComposeFile', () => {
+    let yml
+
+    it('should return fine with version 2', (done) => {
+      yml = `
+version: '2'
+services:
+  web:
+    build: .
+    ports:
+      - "7890:7890"
+`
+      DockerComposeParser._parseDockerComposeFile(yml)
+        .asCallback(done)
+    })
+
+    it('should return fine with version 2.0', (done) => {
+      yml = `
+version: '2.0'
+services:
+  web:
+    build: .
+    ports:
+      - "7890:7890"
+`
+      DockerComposeParser._parseDockerComposeFile(yml)
+        .asCallback(done)
+    })
+
+    it('should fail with version 3', (done) => {
+      yml = `
+version: '3.0'
+services:
+  web:
+    build: .
+    ports:
+      - "7890:7890"
+`
+      DockerComposeParser._parseDockerComposeFile(yml)
+        .then(() => {
+          done(new Error('Should have failed'))
+        })
+        .catch(() => done())
+    })
+    it('should fail with version 2.a', (done) => {
+      yml = `
+version: '2.a'
+services:
+  web:
+    build: .
+    ports:
+      - "7890:7890"
+`
+      DockerComposeParser._parseDockerComposeFile(yml)
+        .then(() => { done(new Error('Should have failed')) })
+        .catch(() => done())
+    })
+  })
+})
