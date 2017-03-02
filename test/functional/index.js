9b6a046ea9896619354ea942cb3bd4ca914e1678
thejsj <jorge.silva@thejsj.com>
2017-03-01 17:22:30 -0800

    Fix tests

--- test/functional/index.js
+++ test/functional/index.js
@@ -534,28 +534,30 @@ describe('5. Links and aliases', () => {
             'alias': 'rethinkdb3',
             'instanceName': 'compose-test-repo-5-1-rethinkdb3'
           },
-          'YW5vdGhlcjEuc3VwZXJfd2VpcmQuMDBob3N0': {
-            'alias': 'another1.super_weird.00host',
-            'instanceName': 'compose-test-repo-5-1-rethinkdb3'
-          },
           'cmV0aGlua2RiNA==': {
             'alias': 'rethinkdb4',
             'instanceName': 'compose-test-repo-5-1-rethinkdb4'
           },
-          'b21nLm5vdC5hbm90aGVyLm9uZQ==': {
-            'alias': 'omg.not.another.one',
+          'dGhyZWUtY2hhbmdpbmctdGhlLWhvc3RuYW1l': {
+            'alias': 'three-changing-the-hostname',
             'instanceName': 'compose-test-repo-5-1-rethinkdb4'
+          },
+          'dGhyZWUtY2hhbmdpbmctdGhlLXdlaXJkLWhvc3Q=': {
+            'alias': 'three-changing-the-weird-host',
+            'instanceName': 'compose-test-repo-5-1-rethinkdb3'
           }
         })
       })
 
       it('should not replace aliases with hostnames', () => {
-        const hostname = `${sanitizeName(repositoryName)}-rethinkdb3-staging-${ownerUsername.toLowerCase()}.${userContentDomain}`
+        const hostname1 = `${sanitizeName(repositoryName)}-rethinkdb3-staging-${ownerUsername.toLowerCase()}.${userContentDomain}`
+        const hostname2 = `${sanitizeName(repositoryName)}-rethinkdb4-staging-${ownerUsername.toLowerCase()}.${userContentDomain}`
         expect(services).to.have.deep.property('[0].instance.env')
         expect(services[0].instance.env).to.deep.equal([
           `PORT=3000`,
-          `RETHINKDB_3_1=${hostname}`,
-          `RETHINKDB_3_2=another1.super_weird.00host`
+          `RETHINKDB_3_1=${hostname1}`,
+          `RETHINKDB_3_2=three-changing-the-weird-host`,
+          `RETHINKDB_4_3=${hostname2}`
         ])
       })
     })
@@ -576,9 +578,10 @@ describe('5. Links and aliases', () => {
         expect(services[0].instance.env).to.deep.equal([
           `PORT=3000`,
           `RETHINKDB_3_1=${hostname1}`,
-          `RETHINKDB_3_2=another1.super_weird.00host`,
+          `RETHINKDB_3_2=three-changing-the-weird-host`,
+          `RETHINKDB_4_3=${hostname2}`,
           `RETHINKDB_4_1=${hostname2}`,
-          `RETHINKDB_4_2=omg.not.another.one`
+          `RETHINKDB_4_2=three-changing-the-hostname`
         ])
       })
     })
